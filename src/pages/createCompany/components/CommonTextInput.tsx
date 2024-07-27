6968775b37cf08a6a3bc3e71eca57b1e4b83ecb7
roman148 <roman@u-coders.team>
2024-07-27 13:45:33 +0300

    added buttons

--- src/pages/createCompany/components/CommonTextInput.tsx
+++ src/pages/createCompany/components/CommonTextInput.tsx
@@ -50,23 +50,25 @@ const CommonTextInput = ({
         >
           {removeLabel ? '' : title}
         </label>
-        <div className="mt-1">
+        <div className="mt-1 relative">
           <input
             {...field}
             onChange={changeHandler}
             type="text"
             name={name}
             id={id}
-            className={
-              extraInputStyles
-                ? 'outline-0 block w-full text-2xl font-bold border-b py-3.5 px-4 text-gray-900 placeholder:text-gray-400 max-sm:text-sm sm:leading-6'
-                : `outline-0 block w-full text-xl rounded-md font-bold border-0 py-3.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-mainBlue max-sm:text-sm sm:leading-6 ${extraInputStyles}`
-            }
+            className={`outline-0 block w-full ${extraInputStyles ? 'text-2xl' : 'text-xl'} rounded-md font-bold border-0 py-3.5 px-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-mainBlue max-sm:text-sm sm:leading-6`}
             placeholder={title}
             data-1p-ignore={true}
             readOnly={readonly}
             disabled={readonly}
           />
+          <button
+            type={'button'}
+            className="absolute right-1.5 top-1.5 bottom-1.5 px-4 bg-mainBlue hover:bg-sideBarBlue transition rounded-md text-white"
+          >
+            Save
+          </button>
         </div>
       </div>
     </>
