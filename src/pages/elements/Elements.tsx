7d367bba35dda4148cbdc98a2008617def8401e7
roman148 <roman@u-coders.team>
2024-08-07 01:29:23 +0300

    fixes

--- src/pages/elements/Elements.tsx
+++ src/pages/elements/Elements.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SectionStatus from './components/SectionStatus';
 import InputWithButton from './components/InputWithButton';
+import JoinedCardsElements from './components/JoinedCardsElements';
 
 const Elements = () => {
   return (
     <div className="p-10">
       <SectionStatus />
       <InputWithButton />
+      <JoinedCardsElements />
     </div>
   );
 };
---
+++ src/pages/elements/components/JoinedCardsElements.tsx
@@ -0,0 +1,24 @@
+import JoinedCard from '../../createCompany/components/JoinedCard';
+import React from 'react';
+
+const companyTypes = [
+  { fullName: 'Corporation', shortName: 'C-corp' },
+  { fullName: 'Limited Liability Company', shortName: 'LLC' },
+  { fullName: 'Non-profit', shortName: 'Non-profit' },
+];
+const JoinedCardsElements = () => {
+  const [state, setState] = React.useState('');
+  return (
+    <div className="w-1/2">
+      <h2 className="mb-4 font-bold">Joined cards</h2>
+      <JoinedCard
+        value={state}
+        changeEvent={setState}
+        state={companyTypes}
+        title={'Joined cards heading'}
+      />
+    </div>
+  );
+};
+
+export default JoinedCardsElements;
