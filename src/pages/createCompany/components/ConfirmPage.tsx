6624a54a0cb9b30961ae3801232a2c8ab6eed9b1
roman148 <roman@u-coders.team>
2024-08-25 23:52:06 +0300

    fixed 921

--- src/pages/createCompany/components/ConfirmPage.tsx
+++ src/pages/createCompany/components/ConfirmPage.tsx
@@ -1,57 +1,78 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { fields } from '../../../constants/form/form';
-import ConfettiAp from '../../../components/shared/Confetti';
+// import ConfettiAp from '../../../components/shared/Confetti';
 
 interface IProps {
   stepOneData: { [key: string]: string };
   stepTwoData: { [key: string]: string };
   setCurrentStep: (value: number) => void;
 }
-//
-// function classNames(...classes: (string | boolean)[]) {
-//   return classes.filter(Boolean).join(' ');
-// }
+
+const statusBadge = (status: string) => {
+  switch (status) {
+    case 'Active':
+      return 'bg-green-300 text-green-800';
+    case 'Inactive':
+      return 'bg-red-300 text-red-800';
+    case 'Dissolved':
+      return 'bg-gray-300 text-gray-800';
+    case 'Withdrawn':
+      return 'bg-gray-300 text-gray-800';
+    default:
+      return 'bg-red-300 text-red-800';
+  }
+};
+
+function classNames(...classes: (string | boolean)[]) {
+  return classes.filter(Boolean).join(' ');
+}
 
 const ConfirmPage = ({ stepOneData, stepTwoData, setCurrentStep }: IProps) => {
-  const [confetti, setConfetti] = React.useState(false);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setConfetti(true);
-    }, 100);
-
-    return () => clearTimeout(timeout);
-  }, []);
+  // const [confetti, setConfetti] = React.useState(false);
 
   return (
     <div>
-      {confetti && <ConfettiAp />}
+      {/*{confetti && <ConfettiAp />}*/}
       <div className="px-4 sm:px-0">
-        <h3 className="text-base font-semibold leading-7 text-gray-900">
+        <h3 className="text-lg font-semibold leading-7 text-gray-900">
           Please review all company details
         </h3>
-        <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+        <p className="mt-1 max-w-2xl text-md leading-6 text-gray-500">
           You can jump between steps to make any necessary changes.
         </p>
       </div>
       <div className="mt-6 border-t border-gray-100">
         <dl className="divide-y divide-gray-100">
           {fields.map((field, index) => {
-            console.log(stepTwoData, 'stepTwoData[field.key]');
+            console.log(field, 'field');
+            const fieldValue =
+              field.step === 0
+                ? stepOneData[field.key]
+                : stepTwoData[field.key];
+
             return (
               <div
                 key={index}
                 className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0"
               >
-                <dt className="text-sm font-medium leading-6 text-gray-900">
+                <dt className="text-lg font-medium leading-6 text-gray-900">
                   {field.name}
                 </dt>
-                <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                  <span className="flex-grow">
-                    {field.step === 0
-                      ? stepOneData[field.key]
-                      : stepTwoData[field.key]}
-                  </span>
+                <dd className="mt-1 flex text-lg leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                  {field.key !== 'status' ? (
+                    <span className="flex-grow">{fieldValue}</span>
+                  ) : (
+                    <span className="flex-grow">
+                      <span
+                        className={classNames(
+                          statusBadge(fieldValue),
+                          'inline-flex items-center rounded-md px-1.5 py-0.5 text-base font-medium'
+                        )}
+                      >
+                        {fieldValue}
+                      </span>
+                    </span>
+                  )}
                   <span className="ml-4 flex-shrink-0">
                     <button
                       type="button"
