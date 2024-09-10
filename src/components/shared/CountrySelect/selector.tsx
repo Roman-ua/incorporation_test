6bc0c8efef79b428d604dfd003b1896be47ca02d
roman148 <roman@u-coders.team>
2024-09-11 01:22:43 +0300

    fixed

--- src/components/shared/CountrySelect/selector.tsx
+++ src/components/shared/CountrySelect/selector.tsx
@@ -3,6 +3,7 @@
 import { SelectMenuOption } from './types';
 import { AnimatePresence, motion } from 'framer-motion';
 import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
 
 export interface CountrySelectorProps {
   id: string;
@@ -100,19 +101,12 @@ export default function CountrySelector({
             }`}
           >
             {!disableDropDown && (
-              <svg
-                className="h-5 w-5 text-gray-400"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <ChevronDownIcon
+                className={classNames(
+                  'w-4 h-4 text-gray-500 transition-all',
+                  open ? 'transform rotate-180' : ''
+                )}
+              />
             )}
           </span>
         </button>
--- src/pages/createCompany/components/USAddressForm.tsx
+++ src/pages/createCompany/components/USAddressForm.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CountrySelector from '../../../components/shared/CountrySelect/selector';
 import { COUNTRIES } from '../../../components/shared/CountrySelect/countries';
 import { SelectMenuOption } from '../../../components/shared/CountrySelect/types';
 import { USStates } from '../../../constants/form/form';
 import { VALIDATORS } from '../../../constants/regexs';
 import { PlusIcon } from '@heroicons/react/24/outline';
+import CheckBox from '../../../components/shared/CheckBox';
 
 function classNames(...classes: (string | boolean)[]) {
   return classes.filter(Boolean).join(' ');
@@ -42,6 +43,9 @@ const areFieldsValid = ({
 
 const USAddressForm = ({ setFromState }: IProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [loader, setLoader] = React.useState(false);
+  const [done, setDone] = React.useState(false);
+
   const [addressFields, setAddressFields] =
     useState<{ title: string; type: string }[]>(addressFieldsMock);
   const [isOpenStates, setIsOpenStates] = useState(false);
@@ -69,22 +73,44 @@ const USAddressForm = ({ setFromState }: IProps) => {
   }>({
     address0: '',
   });
-  console.log(address, 'address');
+
   const [city, setCity] = useState('');
   const [zip, setZip] = useState('');
   const [state, setState] = useState('');
 
   const setZipHandler = (value: string) => {
-    if (value.length === 1 && value[0] === '-') {
-      return;
+    let cleanedValue = value.replace(/[^0-9-]/g, '');
+
+    if (cleanedValue.length > 5 && cleanedValue[5] !== '-') {
+      cleanedValue = cleanedValue.slice(0, 5) + '-' + cleanedValue.slice(5);
     }
-    if (value.length > 5 && value[5] !== '-') {
-      return;
-    }
-    if (VALIDATORS.ZIP_CODE.test(value) || value === '') {
-      setZip(value);
+
+    if (VALIDATORS.ZIP_CODE.test(cleanedValue) || cleanedValue === '') {
+      setZip(cleanedValue);
     }
   };
+
+  const saveHandler = () => {
+    if (
+      areFieldsValid({ country, address0: address.address0, city, zip, state })
+    ) {
+      setLoader(true);
+
+      const timeout = setTimeout(() => {
+        setLoader(false);
+        setDone(true);
+        setFromState({ country, ...address, city, zip, state });
+
+        clearTimeout(timeout);
+      }, 500);
+    }
+  };
+
+  useEffect(() => {
+    if (done) {
+      setDone(false);
+    }
+  }, [address, state, city, country, zip]);
 
   const inputCommonClasses =
     'p-2 text-md border-b focus:outline-none placeholder-gray-500';
@@ -161,7 +187,10 @@ const USAddressForm = ({ setFromState }: IProps) => {
             wrapperExtraStyles={'rounded-none border-t-0 border-l-0'}
           />
           <input
-            className={classNames(inputCommonClasses, 'w-5/12')}
+            className={classNames(
+              inputCommonClasses,
+              'min-w-[110px] max-w-[110px]'
+            )}
             type="text"
             value={zip}
             onChange={(e) => setZipHandler(e.target.value)}
@@ -185,23 +214,55 @@ const USAddressForm = ({ setFromState }: IProps) => {
           wrapperExtraStyles={'rounded-b-0 border-0'}
         />
       </div>
-      <button
-        onClick={() => {
-          setFromState({ country, ...address, city, zip, state });
-        }}
-        disabled={
-          !areFieldsValid({
-            country,
-            address0: address.address0,
-            city,
-            zip,
-            state,
-          })
-        }
-        className="px-4 py-2 text-base font-bold bg-mainBlue text-white rounded-md mt-2 disabled:bg-gray-500"
-      >
-        Save
-      </button>
+      {done ? (
+        <div
+          className={classNames(
+            'rounded-full w-7 flex items-center justify-center mt-2'
+          )}
+        >
+          <CheckBox
+            wrapperSize={'w-7 h-7'}
+            iconSize={'w-3.5 h-3.5'}
+            isItemHovered={false}
+            isItemSelected={true}
+          />
+        </div>
+      ) : (
+        <button
+          onClick={saveHandler}
+          disabled={
+            !areFieldsValid({
+              country,
+              address0: address.address0,
+              city,
+              zip,
+              state,
+            })
+          }
+          className="flex items-center justify-center w-[69px] h-[40px] text-base font-bold bg-mainBlue text-white rounded-md mt-2 disabled:bg-gray-500"
+        >
+          {loader ? (
+            <svg
+              aria-hidden="true"
+              className="w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-white"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+          ) : (
+            'Save'
+          )}
+        </button>
+      )}
     </div>
   );
 };
