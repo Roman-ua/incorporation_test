db078b2cbcdb04ce01966921281c031439e5c723
roman148 <roman@u-coders.team>
2024-09-19 23:45:10 +0300

    964 fixed

--- src/pages/createCompany/CreateCompany.tsx
+++ src/pages/createCompany/CreateCompany.tsx
@@ -452,6 +452,7 @@ const CreateCompany = () => {
                   >
                     Back
                   </button>
+
                   <ButtonWithArrow />
                 </div>
                 <div className="w-1/4 pr-2 max-lg:hidden" />
