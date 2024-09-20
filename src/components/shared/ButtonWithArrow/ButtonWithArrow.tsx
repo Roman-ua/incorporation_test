e61934332f12c3f6475fa078bde8f7abd1467f56
roman148 <roman@u-coders.team>
2024-09-21 00:43:49 +0300

    fixed comments

--- src/components/shared/ButtonWithArrow/ButtonWithArrow.tsx
+++ src/components/shared/ButtonWithArrow/ButtonWithArrow.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import arrow from '../../../images/shared/rightArrow.png';
 // import styles from './ButtonWithArrow.module.scss';
 // function classNames(...classes: (string | boolean)[]) {
 //   return classes.filter(Boolean).join(' ');
@@ -16,48 +17,53 @@ const ButtonWithArrow = () => {
     //   <span className={styles.arrow} />
     // </button>
 
-    <button className="relative inline-flex rounded-md bg-mainBlue items-center justify-start py-2.5 pl-4 pr-12 overflow-hidden font-semibold transition-all duration-150 ease-in-out hover:pl-10 hover:pr-6 group hover:bg-sideBarBlue">
-      <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-        <svg
-          className="w-4 h-5 text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-        >
-          <path
-            d="M14.9385 6L20.9999 12.0613M20.9999 12.0613L14.9385 18.1227M20.9999 12.0613L3 12.0613"
-            stroke="currentcolor"
-            strokeWidth="1.6"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </span>
-      <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-        <svg
-          className="w-4 h-5 text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-        >
-          <path
-            d="M14.9385 6L20.9999 12.0613M20.9999 12.0613L14.9385 18.1227M20.9999 12.0613L3 12.0613"
-            stroke="currentcolor"
-            strokeWidth="1.6"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </span>
+    <button className="relative inline-flex rounded-md bg-mainBlue items-center justify-start py-2.5 pl-4 pr-7 overflow-hidden font-semibold transition-all duration-150 ease-in-out group hover:bg-sideBarBlue">
       <span className="text-sm font-semibold text-white relative w-full text-left transition-colors duration-200 ease-in-out">
         Next Step
       </span>
+      <img
+        className="w-4 translate-x-2 group-hover:translate-x-3 transition-all duration-200 ease-in-out"
+        src={arrow}
+        alt="arrow"
+      />
     </button>
   );
 };
 
 export default ButtonWithArrow;
+// <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+//         <svg
+//           className="w-4 h-5 text-white"
+//           xmlns="http://www.w3.org/2000/svg"
+//           width="24"
+//           height="24"
+//           viewBox="0 0 24 24"
+//           fill="none"
+//         >
+//           <path
+//             d="M14.9385 6L20.9999 12.0613M20.9999 12.0613L14.9385 18.1227M20.9999 12.0613L3 12.0613"
+//             stroke="currentcolor"
+//             strokeWidth="1.6"
+//             strokeLinecap="round"
+//             strokeLinejoin="round"
+//           />
+//         </svg>
+//       </span>
+// <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+//         <svg
+//           className="w-4 h-5 text-white"
+//           xmlns="http://www.w3.org/2000/svg"
+//           width="24"
+//           height="24"
+//           viewBox="0 0 24 24"
+//           fill="none"
+//         >
+//           <path
+//             d="M14.9385 6L20.9999 12.0613M20.9999 12.0613L14.9385 18.1227M20.9999 12.0613L3 12.0613"
+//             stroke="currentcolor"
+//             strokeWidth="1.6"
+//             strokeLinecap="round"
+//             strokeLinejoin="round"
+//           />
+//         </svg>
+//       </span>
