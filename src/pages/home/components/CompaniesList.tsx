9dd4c9478f45ae95558954004dcbc3682a6710aa
roman148 <roman@u-coders.team>
2024-09-28 03:10:53 +0300

    fixed company list

--- src/pages/home/components/CompaniesList.tsx
+++ src/pages/home/components/CompaniesList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '../../../constants/navigation/routes';
+import { ArrowRightIcon } from '@heroicons/react/20/solid';
 
 const mockCompanies = [
   {
@@ -42,7 +43,7 @@ const mockCompanies = [
   {
     registeredIn: 'State of Florida',
     companyName: '5 My Company',
-    companyType: 'Non-Profit',
+    companyType: 'Non-profit',
     registrationDate: 'September 10, 2024',
     registrationNumber: '123-433R',
     status: 'Active',
@@ -82,47 +83,34 @@ const CompaniesList = () => {
   }, [localData]);
 
   return (
-    // <div className="mt-8 flow-root">
-    //   <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-    //     <div className="inline-block min-w-full py-2 align-middle">
-    //
-    //     </div>
-    //   </div>
-    // </div>
     <table className="min-w-full divide-y divide-gray-300">
       <thead>
         <tr>
           <th
             scope="col"
-            className="px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+            className="py-3 pl-3 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
           >
             Company Name
           </th>
           <th
             scope="col"
-            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+            className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
+          >
+            Status
+          </th>
+          <th
+            scope="col"
+            className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
           >
             Type
           </th>
           <th
             scope="col"
-            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+            className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
           >
-            Reg. Number
+            State
           </th>
-          <th
-            scope="col"
-            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-          >
-            Reg. State
-          </th>
-          <th
-            scope="col"
-            className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 w-20"
-          >
-            Status
-          </th>
-          <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+          <th scope="col" className="relative py-3 pl-3 pr-4 sm:pr-0">
             <span className="sr-only">Edit</span>
           </th>
         </tr>
@@ -132,44 +120,30 @@ const CompaniesList = () => {
           <tr
             key={company.id}
             onClick={() => navigate(`${ROUTES.COMPANY}`)}
-            className="hover:bg-gray-100 transition-all duration-150 ease-in-out hover:cursor-pointer"
+            className="hover:bg-gray-100 transition-all duration-150 ease-in-out hover:cursor-pointer group"
           >
-            <td className="whitespace-nowrap py-5 px-2">
-              <div className="flex items-center">
-                <div>
-                  <div className="font-semibold text-gray-700 text-lg">
-                    {company.companyName}
-                  </div>
-                  <div className="mt-1 text-gray-500 text-sm">
-                    {company.registrationDate}
-                  </div>
-                </div>
-              </div>
+            <td className="whitespace-nowrap py-4 pl-3 pr-3 text-base font-semibold text-gray-700">
+              {company.companyName}
             </td>
-            <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-              <div className="text-gray-700">{company.companyType}</div>
-            </td>
-            <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-              <div className="text-gray-700 font-semibold">
-                {company.registrationNumber}
-              </div>
-            </td>
-            <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
-              <div className="text-gray-700">{company.registeredIn}</div>
-            </td>
-            <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500 w-20">
+            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
               <span
                 className={classNames(
-                  'inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ring-green-600/20',
+                  'inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset',
                   statusBadge(company.status)
                 )}
               >
                 {company.status}
               </span>
             </td>
-            <td className="relative whitespace-nowrap py-5 px-2 text-right text-sm font-medium">
-              <div className="text-mainBlue hover:text-sideBarBlue">
-                Details
+            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-700">
+              {company.companyType}
+            </td>
+            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-700">
+              {company.registeredIn}
+            </td>
+            <td className="relative opacity-0 group-hover:opacity-100 whitespace-nowrap py-5 px-2 text-right text-sm font-medium transition-all duration-150 ease-in-out">
+              <div className="p-1 rounded w-fit ml-auto bg-mainBlue text-white hover:bg-gray-700 transition-all duration-150 ease-in-out">
+                <ArrowRightIcon className="h-4 w-4" />
                 <span className="sr-only">, {company.companyName}</span>
               </div>
             </td>
--- src/pages/home/components/SectionHeadeing.tsx
+++ src/pages/home/components/SectionHeadeing.tsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import { ROUTES } from '../../../constants/navigation/routes';
 import { useNavigate } from 'react-router-dom';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
-interface IProps {
-  title: string;
-}
-
-const SectionHeading = ({ title }: IProps) => {
+const SectionHeading = () => {
   const navigate = useNavigate();
   const handleButtonClick = () => {
     navigate(ROUTES.CREATE_COMPANY);
   };
   return (
-    <div className="sm:flex sm:items-start mb-8">
-      <div className="sm:flex-auto">
-        <h1 className="text-base font-semibold leading-6 text-gray-900">
-          {title}
-        </h1>
-        <p className="mt-2 text-sm text-gray-700">
-          A list of all the companies in your account.
-        </p>
-      </div>
+    <div className="sm:flex sm:items-center border-b pb-3 mb-11">
+      <form action="#" method="GET" className="relative flex flex-1">
+        <label htmlFor="search-field" className="sr-only">
+          Search
+        </label>
+        <MagnifyingGlassIcon
+          aria-hidden="true"
+          className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
+        />
+        <input
+          id="search-field"
+          name="search"
+          type="search"
+          placeholder="Search..."
+          className="outline-0 block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-700 placeholder:text-gray-400 focus:ring-0 sm:text-base"
+        />
+      </form>
       <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
         <button
           type="button"
           onClick={handleButtonClick}
           className="block rounded-md bg-mainBlue px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-sideBarBlue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
         >
-          Create new
+          + New Company
         </button>
       </div>
     </div>
