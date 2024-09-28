154d665a796ba9e8c76a5a6e1b1007968780efb1
roman148 <roman@u-coders.team>
2024-09-28 03:44:44 +0300

    added company page

--- src/pages/company/CompanyPage.tsx
+++ src/pages/company/CompanyPage.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import SectionHeading from './components/SectionHeading';
 const CompanyPage = () => {
-  return (
-    <div>
-      <div>CompanyPage</div>
+  const localData = localStorage.getItem('finalFormData');
+  const data = localData ? JSON.parse(localData) : undefined;
+
+  const rowClass = 'text-sm text-gray-500 mb-1';
+  const rowValueClass = 'text-sm text-gray-700 mb-1';
+
+  return data ? (
+    <div className="w-full pl-10 pr-10 pt-5">
+      <SectionHeading title="General Information" />
+      <div className="grid grid-cols-2 w-1/2 gap-4 mb-11">
+        <div>
+          <div className={rowClass}>Company Name</div>
+          <div className={rowClass}>Status</div>
+          <div className={rowClass}>Company Type</div>
+          <div className={rowClass}>State</div>
+          <div className={rowClass}>Registration Date</div>
+          <div className={rowClass}>Registration Number</div>
+        </div>
+        <div>
+          <div className={rowValueClass}>{data.companyName}</div>
+          <div className={rowValueClass}>{data.status}</div>
+          <div className={rowValueClass}>{data.companyType}</div>
+          <div className={rowValueClass}>{data.registeredIn}</div>
+          <div className={rowValueClass}>{data.registrationDate}</div>
+          <div className={rowValueClass}>{data.registrationNumber}</div>
+        </div>
+      </div>
+      <SectionHeading title="Primary Address" />
+      <div className="grid grid-cols-2 w-1/2 gap-4 mb-11">
+        <div>
+          <div className={rowClass}>Street</div>
+          <div className={rowClass}>Unit</div>
+          <div className={rowClass}>City</div>
+          <div className={rowClass}>State</div>
+          <div className={rowClass}>Country</div>
+          <div className={rowClass}>Zip</div>
+        </div>
+        <div>
+          <div className={rowValueClass}>{data.address.address0}</div>
+          <div className={rowValueClass}>{data.address.address1}</div>
+          <div className={rowValueClass}>{data.address.city}</div>
+          <div className={rowValueClass}>{data.address.state}</div>
+          <div className={rowValueClass}>{data.address.country}</div>
+          <div className={rowValueClass}>{data.address.zip}</div>
+        </div>
+      </div>
     </div>
+  ) : (
+    <></>
   );
 };
 
---
+++ src/pages/company/components/SectionHeading.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+interface IProps {
+  title: string;
+}
+
+const SectionHeading = ({ title }: IProps) => {
+  return (
+    <div className="w-full border-b text-lg font-semibold text-gray-700 mb-3 pb-1">
+      {title}
+    </div>
+  );
+};
+
+export default SectionHeading;
