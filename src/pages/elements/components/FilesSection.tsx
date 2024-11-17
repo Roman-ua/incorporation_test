c347f917a5d28e6c6c21935f5ddfa9cbdae677d6
roman148 <roman@u-coders.team>
2024-11-17 09:46:30 +0200

    1070

--- src/pages/elements/components/FilesSection.tsx
+++ src/pages/elements/components/FilesSection.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import SectionHeading from '../../createCompany/components/SectionHeading';
-import { PaperClipIcon } from '@heroicons/react/20/solid';
+// import { PaperClipIcon } from '@heroicons/react/20/solid';
 import { TbTrash } from 'react-icons/tb';
 import { MdOutlineCloudDownload } from 'react-icons/md';
+import {
+  IconFileTypeDoc,
+  IconFileTypeJpg,
+  IconFileTypePdf,
+  IconFileTypeXls,
+} from '@tabler/icons-react';
 
 const mockFiles = [
   {
     id: 1,
     icon: 'pdf',
-    name: 'File One',
+    name: 'File One long name example',
     label: 'CP575A',
     type: 'pdf',
-    date: '2021-05-23',
+    date: 'November 23, 2021',
   },
   {
     id: 2,
@@ -19,7 +25,7 @@ const mockFiles = [
     name: 'File Two',
     label: 'Screenshot',
     type: 'jpg',
-    date: '2021-05-23',
+    date: 'November 23, 2021',
   },
   {
     id: 3,
@@ -27,7 +33,7 @@ const mockFiles = [
     name: 'File Three',
     label: 'CP575G',
     type: 'xls',
-    date: '2021-05-23',
+    date: 'November 23, 2021',
   },
   {
     id: 4,
@@ -35,7 +41,7 @@ const mockFiles = [
     name: 'File Four',
     label: '147C',
     type: 'xlsx',
-    date: '2021-05-23',
+    date: 'November 23, 2021',
   },
   {
     id: 5,
@@ -43,7 +49,7 @@ const mockFiles = [
     name: 'File Five',
     label: 'Faxed SS-4',
     type: 'xls',
-    date: '2021-05-23',
+    date: 'November 23, 2021',
   },
   {
     id: 6,
@@ -51,7 +57,7 @@ const mockFiles = [
     name: 'File Six',
     label: 'W-9',
     type: 'pdf',
-    date: '2021-05-23',
+    date: 'November 23, 2021',
   },
   {
     id: 7,
@@ -59,7 +65,7 @@ const mockFiles = [
     name: 'File Seven',
     label: 'CP577',
     type: 'jpg',
-    date: '2021-05-23',
+    date: 'November 23, 2021',
   },
   {
     id: 8,
@@ -67,33 +73,30 @@ const mockFiles = [
     name: 'File Eight',
     label: 'CP577E',
     type: 'pdf',
-    date: '2021-05-23',
+    date: 'November 23, 2021',
   },
 ];
-const replaceSpaces = (str: string) => str.replace(/ /g, '_');
 
-const labelBadgeHandler = (label: string) => {
-  switch (label) {
-    case 'CP575A':
-      return 'bg-red-100 text-red-700 ring-red-600/20';
-    case 'Screenshot':
-      return 'bg-yellow-100 text-yellow-700 ring-yellow-600/20';
-    case 'CP575G':
-      return 'bg-blue-100 text-blue-700 ring-blue-600/20';
-    case '147C':
-      return 'bg-purple-100 text-purple-700 ring-purple-600/20';
-    case 'Faxed SS-4':
-      return 'bg-green-100 text-green-700 ring-green-600/20';
-    case 'W-9':
-      return 'bg-red-100 text-red-700 ring-red-600/20';
-    case 'CP577':
-      return 'bg-yellow-100 text-yellow-700 ring-yellow-600/20';
-    case 'CP577E':
-      return 'bg-red-100 text-red-700 ring-red-600/20';
+const fileIconHandler = (type: string) => {
+  switch (type) {
+    case 'pdf':
+      return <IconFileTypePdf className="w-6 h-6 text-gray-500 mr-2" />;
+    case 'jpg':
+      return <IconFileTypeJpg className="w-6 h-6 text-gray-500 mr-2" />;
+    case 'xls':
+      return <IconFileTypeXls className="w-6 h-6 text-gray-500 mr-2" />;
     default:
-      return 'bg-blue-100 text-blue-700 ring-blue-600/20';
+      return <IconFileTypeDoc className="w-6 h-6 text-gray-500 mr-2" />;
   }
 };
+const replaceSpaces = (str: string) => {
+  const result = str.replace(/ /g, '_');
+  if (result.length > 10) {
+    return result.slice(0, 15) + '...';
+  }
+  return result;
+};
+
 function classNames(...classes: (string | boolean)[]) {
   return classes.filter(Boolean).join(' ');
 }
@@ -109,13 +112,10 @@ const FilesSection = () => {
               key={file.id}
               className="flex items-center justify-start py-4 px-2 border-b"
             >
-              <PaperClipIcon
-                aria-hidden="true"
-                className="mr-3 size-5 shrink-0 text-gray-400"
-              />
+              {fileIconHandler(file.type)}
               <div className="flex items-center justify-between w-full pr-4 mr-4 border-r group">
                 <div className="flex min-w-0 items-center flex-1 gap-3">
-                  <span className="truncate font-semibold hover:cursor-pointer">
+                  <span className="truncate hover:cursor-pointer">
                     {replaceSpaces(file.name)}
                   </span>
                   <span className="shrink-0 text-gray-400">{file.date}</span>
@@ -123,17 +123,15 @@ const FilesSection = () => {
                 </div>
                 <div
                   className={classNames(
-                    'flex items-center text-sm px-2 font-medium rounded ring-1 ring-inset leading-6',
-                    labelBadgeHandler(file.label)
+                    'text-nowrap flex items-center text-xs px-2 font-medium rounded ring-1 ring-inset leading-5 mr-1',
+                    'bg-gray-100 text-gray-700 ring-gray-600/20'
                   )}
                 >
                   {file.label}
                 </div>
               </div>
               <div className="ml-auto flex items-center justify-end">
-                <div className="px-1.5 py-1 rounded bg-gray-100 border group hover:cursor-pointer hover:bg-red-100">
-                  <TbTrash className="w-4 h-4 text-gray-500 group-hover:text-gray-700 transition-all ease-in-out duration-150" />
-                </div>
+                <TbTrash className="w-4 h-4 text-gray-500 hover:cursor-pointer hover:text-red-700 transition-all ease-in-out duration-150" />
               </div>
             </div>
           );
