8a4dd59d950d2b49aa3bcf3db593b4cb45bf1155
roman148 <roman@u-coders.team>
2024-11-30 23:36:55 +0200

    1010'

--- src/pages/company/CompanyPage.tsx
+++ src/pages/company/CompanyPage.tsx
@@ -10,7 +10,22 @@ import { ROUTES } from '../../constants/navigation/routes';
 import { IoMdCheckmark } from 'react-icons/io';
 import PageSign from '../../components/shared/PageSign';
 import { HiOutlineOfficeBuilding } from 'react-icons/hi';
-
+import { LuArrowUpRight } from 'react-icons/lu';
+const people = [
+  {
+    name: 'Lindsay Walton',
+    title: 'Accountant',
+    email: 'lindsay.walton@example.com',
+    role: 'Member',
+  },
+  {
+    name: 'Clark Kent',
+    title: 'CTO',
+    email: 'clark.kent@example.com',
+    role: 'Owner',
+  },
+  // More people...
+];
 const statusBadge = (status: string) => {
   switch (status) {
     case 'Active':
@@ -162,6 +177,88 @@ const CompanyPage = () => {
           </div>
           <div>{data.address.country}</div>
         </>
+      </div>
+      {/*<SectionHeading title="Associated users" />*/}
+      <div className="mt-6 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y ">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    Associated users
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Position
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Status
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Role
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200 bg-white">
+                {people.map((person) => (
+                  <tr
+                    key={person.email}
+                    className="transition-all ease-in-out duration-150 group"
+                  >
+                    <td className="whitespace-nowrap py-5 pl-4 pr-3 text-sm sm:pl-0">
+                      <div className="flex items-center">
+                        <div className="size-11 shrink-0">
+                          <span className="w-10 h-10 text-xl font-bold text-white bg-gray-300 rounded-full flex items-center justify-center">
+                            {person.name[0]}
+                          </span>
+                        </div>
+                        <div className="ml-4">
+                          <div className="font-medium text-gray-900">
+                            {person.name}
+                          </div>
+                          <div className="mt-1 text-gray-500">
+                            {person.email}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                      <div className="text-gray-900">{person.title}</div>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                      <span className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+                        Active
+                      </span>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-5 text-sm text-gray-500">
+                      {person.role}
+                    </td>
+                    <td className="relative whitespace-nowrap py-5 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                      <div className="p-1 rounded w-fit ml-auto bg-gray-700 text-white hover:bg-gray-900 transition-all duration-150 ease-in-out hover:cursor-pointer opacity-0 group-hover:opacity-100">
+                        <LuArrowUpRight className="h-4 w-4" />
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   ) : (
