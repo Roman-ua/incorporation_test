0e653bd3a08bc948a9b40d45c57ca3f8d38f5bbf
roman148 <roman@u-coders.team>
2024-12-03 22:23:36 +0200

    1010

--- src/pages/company/components/SectionHeading.tsx
+++ src/pages/company/components/SectionHeading.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 interface IProps {
   title: string;
+  extraClass?: string;
 }
 
-const SectionHeading = ({ title }: IProps) => {
+function classNames(...classes: (string | boolean)[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+const SectionHeading = ({ title, extraClass }: IProps) => {
   return (
-    <div className="w-full border-b text-lg font-semibold text-gray-700 mb-3 pb-1">
+    <div
+      className={classNames(
+        extraClass ? extraClass : '',
+        'w-full border-b text-lg font-semibold text-gray-700 mb-3 pb-1'
+      )}
+    >
       {title}
     </div>
   );
