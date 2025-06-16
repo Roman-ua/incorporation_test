6cdd1a04ec2285347ff29bcf15e9ac4290273e29
roman <roman.havrylchuk@onix-systems.com>
2025-06-16 23:43:34 +0300

    fix

--- src/pages/comingSoon/ComingSoon.tsx
+++ src/pages/comingSoon/ComingSoon.tsx
@@ -4,7 +4,7 @@ import cmgSoon from '../../images/shared/comingsoon.png';
 const ComingSoon = () => {
   return (
     <div className="flex flex-col items-center justify-center p-10">
-      <img src={cmgSoon} alt="logo" className="w-2/12" />
+      <img src={cmgSoon} alt="logo" className="w-[20%]" />
       <h1 className="text-xl font-bold">Coming Soon...</h1>
     </div>
   );
