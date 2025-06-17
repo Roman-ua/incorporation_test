90df9e02c60b98f4726c2e787d0b0c46cab4cd8b
roman <roman.havrylchuk@onix-systems.com>
2025-06-17 23:01:38 +0300

    fix

--- src/pages/Internal/Users/components/Table.tsx
+++ src/pages/Internal/Users/components/Table.tsx
@@ -91,11 +91,9 @@ const statusBadge = (status: string) => {
   switch (status) {
     case 'Active':
       return 'bg-green-50 text-green-700 ring-green-600/20';
+    case 'Pending':
+      return 'bg-orange-50 text-orange-700 ring-orange-600/20';
     case 'Inactive':
-      return 'bg-red-50 text-red-700 ring-red-600/20';
-    case 'Dissolved':
-      return 'bg-gray-50 text-gray-700 ring-gray-600/20';
-    case 'Withdrawn':
       return 'bg-gray-50 text-gray-700 ring-gray-600/20';
     default:
       return 'bg-red-50 text-red-700 ring-red-600/20';
