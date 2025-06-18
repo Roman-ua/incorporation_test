cf2194897d3295406f0bef3e7a0c5e939a432b57
roman <roman.havrylchuk@onix-systems.com>
2025-06-18 16:03:35 +0300

    fix

--- src/pages/workspaces/components/WorkspacesListHeader.tsx
+++ src/pages/workspaces/components/WorkspacesListHeader.tsx
@@ -43,44 +43,44 @@ const WorkspacesListHeader = () => {
         <div className="text-xs text-gray-600 p-2">
           Related to all companies
         </div>
-        {menuItems.map((item, index) => {
-          const Icon = item.icon;
-          return (
-            <div
-              className={classNames(
-                'hover:cursor-pointer font-semibold hover:bg-gray-100/80 text-gray-900 flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left outline-none ring-sidebar-ring transition-[width,height,padding] focus-visible:ring-2 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 h-9 text-sm'
-              )}
-              key={index}
-            >
-              <Icon className={`h-5 w-5 text-gray-900`} />
-              {item.title}
-            </div>
-          );
-        })}
+        <div className="flex flex-col w-full gap-1">
+          {menuItems.map((item, index) => {
+            const Icon = item.icon;
+            return (
+              <div
+                className={classNames(
+                  'hover:cursor-pointer font-semibold hover:bg-gray-100/80 text-gray-900 flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left outline-none ring-sidebar-ring transition-[width,height,padding] focus-visible:ring-2 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 h-9 text-sm'
+                )}
+                key={index}
+              >
+                <Icon className={`h-5 w-5 text-gray-900`} />
+                {item.title}
+              </div>
+            );
+          })}
 
-        {/* Internal Section */}
-        {!hideInternal && (
-          <>
-            <h3 className="px-2 text-xs space-y-3 font-medium text-gray-400 tracking-wider mb-2 mt-10">
-              Internal
-            </h3>
-            {internalItems.map((item, index) => {
-              const Icon = item.icon;
-              return (
-                <div
-                  onClick={() => navigate(item.href)}
-                  className={classNames(
-                    'hover:cursor-pointer font-semibold hover:bg-gray-100/80 text-gray-900 flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left outline-none ring-sidebar-ring transition-[width,height,padding] focus-visible:ring-2 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 h-9 text-sm'
-                  )}
-                  key={index}
-                >
-                  <Icon className={`h-5 w-5 text-gray-900`} />
-                  {item.label}
-                </div>
-              );
-            })}
-          </>
-        )}
+          {/* Internal Section */}
+          {!hideInternal && (
+            <>
+              <div className="text-xs text-gray-600 p-2 mt-10">Internal</div>
+              {internalItems.map((item, index) => {
+                const Icon = item.icon;
+                return (
+                  <div
+                    onClick={() => navigate(item.href)}
+                    className={classNames(
+                      'hover:cursor-pointer font-semibold hover:bg-gray-100/80 text-gray-900 flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left outline-none ring-sidebar-ring transition-[width,height,padding] focus-visible:ring-2 group-has-[[data-sidebar=menu-action]]/menu-item:pr-8 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0 h-9 text-sm'
+                    )}
+                    key={index}
+                  >
+                    <Icon className={`h-5 w-5 text-gray-900`} />
+                    {item.label}
+                  </div>
+                );
+              })}
+            </>
+          )}
+        </div>
       </div>
       <div className="mt-auto">
         <WorkspaceListUserButton />
